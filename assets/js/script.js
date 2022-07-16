793b11f27aef9d69109ec9d73ad9322c47116399
neksebo <54643779+neksebo@users.noreply.github.com>
2022-07-16 11:06:18 -0400

    changes to html and script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,86 +1,109 @@
 function initMap() {
-  const map = new google.maps.Map(document.getElementById('map'), {
+  const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
     center: { lat: 41, lng: -87 },
   });
 
-  var input = document.getElementById('searchInput');
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+  var input = document.getElementById("searchInput");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
 
-    var autocomplete = new google.maps.places.Autocomplete(input);
-    autocomplete.bindTo('bounds', map);
+  var autocomplete = new google.maps.places.Autocomplete(input);
+  autocomplete.bindTo("bounds", map);
 
-    var infowindow = new google.maps.InfoWindow();
-    var marker = new google.maps.Marker({
-        map: map,
-        anchorPoint: new google.maps.Point(0, -29)
+  var infowindow = new google.maps.InfoWindow();
+  var marker = new google.maps.Marker({
+    map: map,
+    anchorPoint: new google.maps.Point(0, -29),
+  });
+
+  autocomplete.addListener("place_changed", function () {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    console.log({ place });
+    if (!place.geometry) {
+      window.alert("Autocomplete's returned place contains no geometry");
+      return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
+    marker.setIcon({
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(35, 35),
     });
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
 
-    autocomplete.addListener('place_changed', function() {
-        infowindow.close();
-        marker.setVisible(false);
-        var place = autocomplete.getPlace();
-        if (!place.geometry) {
-            window.alert("Autocomplete's returned place contains no geometry");
-            return;
-        }
-  
-        // If the place has a geometry, then present it on a map.
-        if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-        } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);
-        }
-        marker.setIcon(({
-            url: place.icon,
-            size: new google.maps.Size(71, 71),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(17, 34),
-            scaledSize: new google.maps.Size(35, 35)
-        }));
-        marker.setPosition(place.geometry.location);
-        marker.setVisible(true);
-    
-        var address = '';
-        if (place.address_components) {
-            address = [
-              (place.address_components[0] && place.address_components[0].short_name || ''),
-              (place.address_components[1] && place.address_components[1].short_name || ''),
-              (place.address_components[2] && place.address_components[2].short_name || '')
-            ].join(' ');
-        }
-    
-        infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-        infowindow.open(map, marker);
-      
-        // Location details
-        for (var i = 0; i < place.address_components.length; i++) {
-            if(place.address_components[i].types[0] == 'postal_code'){
-                document.getElementById('postal_code').innerHTML = place.address_components[i].long_name;
-            }
-            if(place.address_components[i].types[0] == 'country'){
-                document.getElementById('country').innerHTML = place.address_components[i].long_name;
-            }
-        }
-        document.getElementById('location').innerHTML = place.formatted_address;
-        document.getElementById('lat').innerHTML = place.geometry.location.lat();
-        document.getElementById('lon').innerHTML = place.geometry.location.lng();
-    });
+    var address = "";
+    if (place.address_components) {
+      address = [
+        (place.address_components[0] &&
+          place.address_components[0].short_name) ||
+          "",
+        (place.address_components[1] &&
+          place.address_components[1].short_name) ||
+          "",
+        (place.address_components[2] &&
+          place.address_components[2].short_name) ||
+          "",
+      ].join(" ");
+    }
+
+    infowindow.setContent(
+      "<div><strong>" + place.name + "</strong><br>" + address
+    );
+    infowindow.open(map, marker);
+
+    // Location details
+    for (var i = 0; i < place.address_components.length; i++) {
+      if (place.address_components[i].types[0] == "postal_code") {
+        document.getElementById("postal_code").innerHTML =
+          place.address_components[i].long_name;
+      }
+      if (place.address_components[i].types[0] == "country") {
+        document.getElementById("country").innerHTML =
+          place.address_components[i].long_name;
+      }
+    }
+    document.getElementById("location").innerHTML = place.formatted_address;
+    document.getElementById("lat").innerHTML = place.geometry.location.lat();
+    document.getElementById("lon").innerHTML = place.geometry.location.lng();
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "18daf76251msh7e93ac0397bbe70p1b0775jsn693b82572462",
+        "X-RapidAPI-Host": "weatherbit-v1-mashape.p.rapidapi.com",
+      },
+    };
+
+    fetch(
+      `https://api.weatherbit.io/v2.0/alerts?lat=${place.geometry.location.lat()}&lon=${place.geometry.location.lng()}&key=4e13b98bdee542e2844bba58757bdc67&include=minutely`
+      // "https://api.weatherbit.io/v2.0/alerts?lat=39.75895&lon=-84.19161&key=4e13b98bdee542e2844bba58757bdc67"
+      // options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        document.getElementById("alertsList").innerHTML =
+          response.alerts[0].severity +
+          " " +
+          response.alerts[0].title +
+          " " +
+          response.alerts[0].expires_utc;
+        // document.getElementById("weather_desc").innerHTML =
+        // response.data[0].weather.description;
+      })
+      .catch((err) => console.error(err));
+  });
 }
-
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '18daf76251msh7e93ac0397bbe70p1b0775jsn693b82572462',
-		'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
-	}
-};
-
-
-fetch('https://weatherbit-v1-mashape.p.rapidapi.com/alerts?lat=33.16&lon=-89.62', options)
-	.then(response => response.json())
-	.then(response => console.log(response))
-	.catch(err => console.error(err));
 
 window.initMap = initMap;
